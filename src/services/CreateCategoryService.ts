8e2b10fc895ecd1aa9e25789133bb9f2b9ea59ec
Rafael Matos <rmatos@keyks.com>
2021-03-19 20:27:17 -0300

    Utilizando o Princípio da Substituição de Liskov (LSP)

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,31 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name : string;
+    description : string;
+}
+
+/**
+ * [X] - Definir o tipo de retorno
+ * [X] - Alterar o retorno de erro
+ * [X] - Acessar o repositorio
+ * 
+*/
+
+class CreateCategoryService{
+    constructor( private categoriesRepository : ICategoriesRepository){}
+
+    execute({ name,description } : IRequest ) : void {
+
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+        if(categoryAlreadyExists){
+            // return response.status(400).json({error : 'Category already exists!'});
+            throw new Error('Category already exists!')
+        }
+        this.categoriesRepository.create({name,description});
+    
+    }
+}
+
+export { CreateCategoryService }
