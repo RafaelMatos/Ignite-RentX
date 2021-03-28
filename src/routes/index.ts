d64adeb8820f1c5acc162b5afdb7a7c281c34776
RafaelMatos <rafael-matos@outlook.com>
2021-03-27 23:14:58 -0300

    Refatorando as rotas

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { categoriesRoutes } from '../routes/categories.routes';
+import { specificationRoutes } from '../routes/specifications.routes';
+
+const router = Router();
+
+router.use("/categories",categoriesRoutes);
+
+router.use('/specifications',specificationRoutes);
+
+export { router };
