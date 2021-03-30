01ce72dd70bf5979f191f8220eca5f722d0bbc21
RafaelMatos <rafael-matos@outlook.com>
2021-03-29 21:59:21 -0300

    Aplicando conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,24 @@
+import fs from 'fs';
+import csvParse from "csv-parse";
+
 class ImportCategoryUseCase {
 
+    /**
+     *
+     * Recebe arquivo do controller/Insomnia
+     * Cria um stream de leitura do arquivo
+     * Passa o caminho do arquivo que quer ler
+     * Pega os pedaços lidos e passa para o parseFile
+    */
 
-    execute( file : any ) {
-        console.log(file)
+    execute( file : Express.Multer.File ) {
+        const stream = fs.createReadStream(file.path);
+        const parseFile = csvParse();
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) =>{
+            console.log(line);
+        });
     }
 }
 
