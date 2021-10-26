dc56ed7b40aada475a1c473691c6932d8c8f09fd
RafaelMatos <rafael-matos@outlook.com>
2021-10-26 05:55:16 -0300

    adding migrations

---
+++ src/database/migrations/1635195666893-CreateCategories.ts
@@ -0,0 +1,37 @@
+import {MigrationInterface, QueryRunner,Table} from "typeorm";
+
+export class CreateCategories1635195666893 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table(
+                {
+                    name:"categories",
+                    columns: [
+                        {
+                            name:"id",
+                            type:"uuid",
+                        },
+                        {
+                            name:"name",
+                            type:"varchar",
+                        },
+                        {
+                            name:"description",
+                            type:"varchar",
+                        },
+                        {
+                            name:"created_at",
+                            type:"timestamp",
+                            default:"now()"
+                        }
+                    ],
+                }
+            )
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
